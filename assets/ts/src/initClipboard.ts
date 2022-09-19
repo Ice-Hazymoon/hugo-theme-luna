290733a5a8d98c5792af94b37fb05f5d57b1ebc6
Ice-Hazymoon <imiku.me@gmail.com>
2022-09-19 19:51:21 +0800

    💩 Optimized copy success prompt

--- assets/ts/src/initClipboard.ts
+++ assets/ts/src/initClipboard.ts
@@ -7,7 +7,13 @@ export default function() {
     }
     this._clipboard = new ClipboardJS('[data-clipboard-text]');
     this._clipboard.on('success', function(e) {
-        alert(window.__theme.i18n.copy.success);
+        // alert(window.__theme.i18n.copy.success);
+        e.trigger.classList.remove('eva-clipboard-outline');
+        e.trigger.classList.add('eva-checkmark-outline');
+        setTimeout(() => {
+            e.trigger.classList.remove('eva-checkmark-outline');
+            e.trigger.classList.add('eva-clipboard-outline');
+        }, 2000);
     })
     this._clipboard.on('error', function(e) {
         alert(window.__theme.i18n.copy.failed);
