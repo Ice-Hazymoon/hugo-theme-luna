f79763414702a81f55a400c1a08ff8e4b96e1171
Ice-Hazymoon <imiku.me@gmail.com>
2022-09-27 23:09:00 +0800

    ✨ Add sub-navigation ✨ Add featured image aspect ratio setting option

--- assets/ts/src/initActiveMenu.ts
+++ assets/ts/src/initActiveMenu.ts
@@ -1,6 +1,7 @@
 export default function() {
-    const activeMenu = document.querySelector('.link-exact-active')
-    if (activeMenu) activeMenu.classList.remove('link-exact-active');
-    const currentMenu = document.querySelector(`[data-active-link="${window.location.pathname.replace(/\/$/, '')}/"]`);
-    if (currentMenu) currentMenu.classList.add('link-exact-active');
+    const activeMenu = document.querySelectorAll('.link-exact-active');
+    if (activeMenu) Array.from(activeMenu).forEach((el) => el.classList.remove('link-exact-active'));
+    const currentMenu = document.querySelectorAll(`[data-active-link="${window.location.pathname.replace(/\/$/, '')}/"]`);
+    console.log(currentMenu);
+    if (currentMenu) Array.from(currentMenu).forEach(el => el.classList.add('link-exact-active'));
 }
